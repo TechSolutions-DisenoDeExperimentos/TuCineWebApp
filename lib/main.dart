d1f0090643c725f90cd8fd086741b9956d65491d
Rafael Primo <u202023137@upc.edu.pe>
2024-06-03 05:55:03 -0500

    fix build to deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:tu_cine_app/config/router/app_router.dart';
 import 'package:tu_cine_app/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  //await dotenv.load(fileName: ".env");
   runApp(
     const ProviderScope(child: MyApp())
   );
